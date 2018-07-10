1f6fd74222e30a32d40de8f497b26afdeaedc5b8
Trevor James Smith <trevor_smith@live.com>
2018-07-10 16:39:13 -0400

    Better testing!

--- pymerra2/__init__.py
+++ pymerra2/__init__.py
@@ -1,7 +1,4 @@
-name = "pymerra2"
-
 __version__ = 0.2
-
 __title__ = 'pymerra2'
 __description__ = 'A tool for downloading and subsetting NASA MERRA-2 Data'
 __url__ = 'https://github.com/Ouranosinc/pymerra2'
@@ -10,5 +7,6 @@ __author_email__ = 'smith.trevorj@ouranos.ca'
 __license__ = 'Apache 2.0'
 __copyright__ = 'Copyright 2018 Ouranos Inc.'
 
-if __name__ == "__main__":
-    pass
+from pymerra2 import download, variables
+
+name = "pymerra2"
--- tests/test_version.py
+++ tests/test_version.py
@@ -1,9 +1,22 @@
-import pymerra2
+import pymerra2 as pm2
+import pytest
 
 
 def test_version_definition():
-    version = pymerra2.__version__
+    version = pm2.__version__
     assert version >= 0.2
+
+def test_modules():
+    import numpy
+    import cftime
+    import Cython
+    import netCDF4
+    import numpy
+    import pypandoc
+    import pytest
+
+def test_variables():
+    assert type(pm2.variables.var_list) == dict
 
 
 if __name__ == '__main__':
